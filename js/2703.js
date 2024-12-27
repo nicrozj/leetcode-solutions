3251059586227988e73ec4dab1207709578317d0
Igor Zhorkin <zhorkinigor@yandex.ru>
2024-12-27 20:44:21 +0300

    solved the 3110th problem

--- js/2703.js
+++ js/2703.js
@@ -1,10 +1,5 @@
-var scoreOfString = function (s) {
-  let chars = s.split("");
-  let sum = 0;
-  for (let i = 0; i < s.length - 1; i++) {
-    sum += Math.abs(chars[i + 1].charCodeAt(0) - chars[i].charCodeAt(0));
-  }
-  return sum;
+var argumentsLength = function (...args) {
+  return args.length;
 };
 
-console.log(scoreOfString("zaz"));
+console.log(argumentsLength(1, {}, NaN));
