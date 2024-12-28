ac1fe168a11ce79ca2b9e191a5504fe233ae4d73
Igor Zhorkin <zhorkinigor@yandex.ru>
2024-12-28 20:43:18 +0300

    solved 1929 problem

--- js/1929.js
+++ js/1929.js
@@ -1,5 +1,9 @@
-var getConcatenation = function (nums) {
-  return [...nums, ...nums];
+var buildArray = function (nums) {
+  let result = [];
+  nums.forEach((element, index) => {
+    result[index] = nums[nums[index]];
+  });
+  return result;
 };
 
-console.log(getConcatenation([1, 2, 3]));
+console.log(buildArray([0, 2, 1, 5, 3, 4]));
